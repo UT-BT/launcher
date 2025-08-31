7e800c3d082feceabcd947a25571e57fe8579cbf
Seb Veigli <seb.veigli@gmail.com>
2025-08-31 14:26:49 +0200

    feat: update install/patching workflow to be a bit nicer

--- app/index.d.ts
+++ app/index.d.ts
@@ -33,12 +33,12 @@ declare module '*.web' {
 interface Window {
   conveyor: import('@/lib/conveyor/api').ConveyorApi
   utInstall: {
-    onProgress: (cb: (data: { stage?: string; progress?: number }) => void) => void
-    onStatus: (cb: (data: { status: string }) => void) => void
-    onConfirm: (cb: (data: { id: string; title: string; message: string; detail?: string }) => void) => void
+    onProgress: (cb: (data: { stage?: string; progress?: number }) => void) => () => void
+    onStatus: (cb: (data: { status: string }) => void) => () => void
+    onConfirm: (cb: (data: { id: string; title: string; message: string; detail?: string }) => void) => () => void
     respondConfirm: (id: string, accepted: boolean) => void
   }
   utPatch: {
-    onStatus: (cb: (data: { status: string; message?: string; tag?: string }) => void) => void
+    onStatus: (cb: (data: { status: string; message?: string; tag?: string }) => void) => () => void
   }
 }
