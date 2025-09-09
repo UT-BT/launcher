a100b8b2b4e68829ade30a6f935788ebfde7e8db
Seb Veigli <seb.veigli@gmail.com>
2025-09-09 22:04:01 +0200

    feat: improve the titlebar design

--- app/renderer.tsx
+++ app/renderer.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import appIcon from '@/resources/build/icon.png'
-import { WindowContextProvider, menuItems } from '@/app/components/window'
+import { WindowContextProvider } from '@/app/components/window'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import App from './app'
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <WindowContextProvider titlebar={{ title: 'UTBT', icon: appIcon, menuItems }}>
+      <WindowContextProvider titlebar={{ title: 'UTBT', icon: appIcon }}>
         <App />
       </WindowContextProvider>
     </ErrorBoundary>
