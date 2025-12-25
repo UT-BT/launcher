e65505901c54dd2d0d7a2c4f4cae4ece9cc213bc
Seb Veigli <seb.veigli@gmail.com>
2025-12-25 15:33:33 +0100

    fix: set executable name to utbt.exe & use our logo

--- lib/main/main.ts
+++ lib/main/main.ts
@@ -3,6 +3,8 @@ import { electronApp, optimizer } from '@electron-toolkit/utils'
 import { createAppWindow } from './app'
 import { loggingService } from '@/lib/main/logging-service'
 
+app.setName('UTBT')
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
